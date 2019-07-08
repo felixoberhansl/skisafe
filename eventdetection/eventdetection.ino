0e136250cc72eb05d30dfa172c99646cfea187b0
Nils Steinger <git@n-st.de>
2019-07-08 15:15:05 +0200

    Skeleton for state machine

--- eventdetection/eventdetection.ino
+++ eventdetection/eventdetection.ino
@@ -4,26 +4,13 @@
 #include <SPI.h>
 #include <SparkFunLSM9DS1.h>
 
-//////////////////////////
-// LSM9DS1 Library Init //
-//////////////////////////
-// Use the LSM9DS1 class to create an object. [imu] can be
-// named anything, we'll refer to that throught the sketch.
-LSM9DS1 imu;
-
-///////////////////////
-// Example I2C Setup //
-///////////////////////
+// LSM9DS1 Library Init:
 // SDO_XM and SDO_G are both pulled high, so our addresses are:
 #define LSM9DS1_M	0x1E // Would be 0x1C if SDO_M is LOW
 #define LSM9DS1_AG	0x6B // Would be 0x6A if SDO_AG is LOW
-
-////////////////////////////
-// Sketch Output Settings //
-////////////////////////////
-#define PRINT_CALCULATED
-//#define PRINT_RAW
-
+// Use the LSM9DS1 class to create an object. [imu] can be
+// named anything, we'll refer to that throught the sketch.
+LSM9DS1 imu;
 
 // Earth's magnetic field varies by location. Add or subtract
 // a declination to get a more accurate heading. Calculate
@@ -31,9 +18,29 @@ LSM9DS1 imu;
 // http://www.ngdc.noaa.gov/geomag-web/#declination
 #define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
 
+enum State {Idle, EventDetected, EventEnded, CheckIsDue};
+enum State state = Idle;
+
+#define THR_EVENT_START 0.15
+#define THR_EVENT_ENDED 0.02
+#define AFTER_EVENT_DELAY 2000
+#define RADIO_PACKET_INTERVAL 20000
+#define THR_AXIS_TILT 0.2
+
 #define HIST_LENGTH 100
 float accelHist[HIST_LENGTH];
 int accelHistIdx = 0;
+float axisHist[HIST_LENGTH];
+int axisHistIdx = 0;
+
+unsigned long lastRadioPacketMillis;
+unsigned long eventEndedMillis;
+
+float variance;
+float axisMean;
+float axisBefore;
+
+#define VERTICAL_AXIS ay
 
 void setup()
 {
@@ -67,7 +74,33 @@ void setup()
 
 void loop()
 {
-    // Update the sensor values whenever new data is available
+    if (getSensorValues()) {
+        // switch to next state based on current state and possible triggers:
+        switch (state) {
+            case EventDetected:
+                if (variance < THR_EVENT_ENDED) {
+                    processEventEnded();
+                }
+                break;
+            case EventEnded:
+                if (millis() > eventEndedMillis + AFTER_EVENT_DELAY) {
+                    processCheckIsDue();
+                }
+                break;
+            default: // e.g. Idle
+                if (variance > THR_EVENT_START) {
+                    processEventDetected();
+                }
+                break;
+        }
+    }
+
+    if (millis() > lastRadioPacketMillis + RADIO_PACKET_INTERVAL) {
+        sendRadioPacket();
+    }
+}
+
+bool getSensorValues() {
     if ( imu.accelAvailable() )
     {
         // To read from the accelerometer, first call the
@@ -80,10 +113,13 @@ void loop()
         float az = imu.calcAccel(imu.az);
         float absAccel = sqrt(ax*ax + ay*ay + az*az);
 
-        accelHist[accelHistIdx] = absAccel;
         accelHistIdx = (accelHistIdx + 1) % HIST_LENGTH;
+        accelHist[accelHistIdx] = absAccel;
 
-        if ((accelHistIdx % 50) == 0) {
+        axisHistIdx = (axisHistIdx + 1) % HIST_LENGTH;
+        axisHist[axisHistIdx] = VERTICAL_AXIS;
+
+        if (accelHistIdx == (HIST_LENGTH-1)) {
             float sum = 0;
             float sumOfSquares = 0;
             for (int i = 0; i < HIST_LENGTH; ++i) {
@@ -92,10 +128,22 @@ void loop()
             }
             float mean = sum / HIST_LENGTH;
             float meanOfSquares = sumOfSquares / HIST_LENGTH;
-            float variance = sqrt(meanOfSquares - mean*mean);
+            variance = sqrt(meanOfSquares - mean*mean);
 
             Serial.print("Variance = ");
             Serial.print(variance);
+            Serial.println();
+        }
+
+        if (axisHistIdx == (HIST_LENGTH-1)) {
+            float sum = 0;
+            for (int i = 0; i < HIST_LENGTH; ++i) {
+                sum += axisHist[i];
+            }
+            axisMean = sum / HIST_LENGTH;
+
+            Serial.print("vertical mean = ");
+            Serial.print(axisMean);
             Serial.println();
         }
 
@@ -104,6 +152,40 @@ void loop()
             Serial.print(absAccel);
             Serial.println();
         }
+
+        return true;
     }
+    return false;
 }
 
+void processEventDetected() {
+    state = EventDetected;
+
+    axisBefore = axisMean;
+
+    Serial.print("Event! |a| = ");
+    Serial.print(accelHist[accelHistIdx]);
+    Serial.println();
+}
+
+void processEventEnded() {
+    state = EventEnded;
+    eventEndedMillis = millis();
+
+}
+
+void processCheckIsDue() {
+    state = CheckIsDue;
+
+    // compare a_v and a_before.
+    // trigger alarm accordingly
+    if (abs(axisMean - axisBefore) > THR_AXIS_TILT) {
+        Serial.println("TILT!");
+    }
+
+    state = Idle;
+}
+
+void sendRadioPacket() {
+
+}
